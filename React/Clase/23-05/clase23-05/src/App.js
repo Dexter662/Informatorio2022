44089824bffbd66a651d3003f528c3b7f806c54b
Dexter662 <kuroookami.662@gmail.com>
2022-05-25 08:05:48 -0300

    Aplicacion de state

--- React/Clase/23-05/clase23-05/src/App.js
+++ React/Clase/23-05/clase23-05/src/App.js
@@ -5,6 +5,7 @@ import Contenido from './Componentes/Contenido';
 import Footer from './Componentes/Footer';
 
 function App() {
+  
   return (
     <div className="App">
       <Header/>
--- React/Clase/23-05/clase23-05/src/Componentes/Contenido.js
+++ React/Clase/23-05/clase23-05/src/Componentes/Contenido.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
-import {Container, CardGroup} from 'react-bootstrap';
+import {Container, CardGroup, Card, Button} from 'react-bootstrap';
 import Contacto from './Contacto';
-import Bloque from './Bloque';
+import img from '../Assets/images/intro.jpg';
+import img2 from '../Assets/images/desarrollo.jpg';
+import img3 from '../Assets/images/especializacion.jpeg';
 
 class Contenido extends Component {
+
+    state = {
+        titulo: "Introduccion a la programación",
+        texto: "Informatorio 2022",
+        titulo2: "Programacion y Desarrollo Web",
+        texto2: "Informatorio 2022",
+        titulo3: "Especialización en Tecnologías de Desarrollo",
+        texto3: "Informatorio 2022"
+    };
 
     render(){
         
@@ -12,9 +23,30 @@ class Contenido extends Component {
             <Container>
                 <br />
                 <CardGroup>
-                    <Bloque />
-                    <Bloque />
-                    <Bloque />
+                    <Card style={{ width: '18rem', margin: '10px', border: 'solid 1px' }}>
+                    <Card.Img style={{ width: '380px', height: '300px', margin: '15px'}} variant="top" src={img} />
+                    <Card.Body>
+                        <Card.Title>{this.state.titulo}</Card.Title>
+                        <Card.Text>{this.state.texto}</Card.Text>
+                        <Button variant="primary">Leer Mas ...</Button>
+                    </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem', margin: '10px', border: 'solid 1px' }}>
+                    <Card.Img style={{ width: '380px', height: '300px', margin: '15px' }} variant="top" src={img2} />
+                    <Card.Body>
+                        <Card.Title>{this.state.titulo2}</Card.Title>
+                        <Card.Text>{this.state.texto2}</Card.Text>
+                        <Button variant="primary">Leer Mas ...</Button>
+                    </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem', margin: '10px', border: 'solid 1px' }}>
+                    <Card.Img style={{ width: '380px', height: '300px', margin: '15px' }} variant="top" src={img3} />
+                    <Card.Body>
+                        <Card.Title>{this.state.titulo3}</Card.Title>
+                        <Card.Text>{this.state.texto3}</Card.Text>
+                        <Button variant="primary">Leer Mas ...</Button>
+                    </Card.Body>
+                    </Card>
                 </CardGroup>
                 <br />
                 <hr />
