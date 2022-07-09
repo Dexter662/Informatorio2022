3b3f0292b22d6469f4f6de67f08d518cb0d0c822
Dexter662 <kuroookami.662@gmail.com>
2022-07-09 01:20:49 -0300

    Ejercicio 3 level 2 Corregido

--- Java/level2/Ejercicio3.java
+++ Java/level2/Ejercicio3.java
@@ -1,19 +1,30 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.Random;
 
 public class Ejercicio3 {
     public static void main(String[] args){
-        int cantidadDeCartas = 52;
-        List<Integer> baraja = new ArrayList<>();
-        for (int i = 1; i <= cantidadDeCartas; i++) {
-            baraja.add(i);
+        String[] palos = {"corazon", "diamante", "trébol", "pino"};
+        String[] valores = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+
+        ArrayList<String> baraja = new ArrayList<String>();
+
+        for (int palo = 0; palo < 4; palo++) {
+            for (int valor = 0; valor < valores.length; valor++) {
+                String carta = valores[valor]+" "+palos[palo];
+                baraja.add(carta);
+            }
         }
+
+        System.out.println(baraja);
         Collections.sort(baraja);
-        System.out.println("Cartas en la baraja: " + baraja);
+        System.out.println("Cartas en la baraja: ");
+        System.out.println(baraja);
         Collections.reverse(baraja);
-        System.out.println("Orden inverso de la baraja: " + baraja);
-        Collections.shuffle(baraja);
-        System.out.println("Baraja mezclada: " + baraja);
+        System.out.println("Orden inverso de la baraja: ");
+        System.out.println(baraja);
+        Collections.shuffle(baraja, new Random());
+        System.out.println("Baraja mezclada: ");
+        System.out.println(baraja);
     }
 }
